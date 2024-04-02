40cb43c3dde7c2093c5a0f4c4aafd75a0b6e87f4
miguel-merlin <mmerlin@stevens.edu>
2024-04-02 09:13:52 -0400

    feat: Added Skills

---
+++ execution/skills/Skill.py
@@ -0,0 +1,16 @@
+class Skill():
+    def __init__(self,
+                name, 
+                logical_preconditions, 
+                expected_logical_effects, 
+                visuomotor_policy, 
+                termination_policy
+                ):
+        self.name = name
+        self.logical_preconditions = logical_preconditions
+        self.expected_logical_effects = expected_logical_effects
+        self.visuomotor_policy = visuomotor_policy
+        self.termination_policy = termination_policy
+
+    def __str__(self):
+        return self.name
