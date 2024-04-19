1292e0221eff8afb617a1173a7e7c3deb85d49b7
miguel-merlin <mmerlin@stevens.edu>
2024-04-18 23:35:13 -0400

    feat: Converted SymbolicPlanner to singleton

--- execution/SymbolicPlanner.py
+++ execution/SymbolicPlanner.py
@@ -1,10 +1,17 @@
 from Model import Model
 
 class SymbolicPlanner(Model):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SymbolicPlanner, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self, model) -> None:
         super().__init__(model)
     
-    def get_scene_predicates(o, num_blocks):
+    def get_scene_predicates(self, o, num_blocks):
         """
         Get scene predicates
         - o: Point cloud observation
